7aeefb9fd065210ce7bc21c6a88c49e99ee652bd
Yuri Yatsenko <115614039+YuriiYatsenko@users.noreply.github.com>
2023-04-16 12:02:23 +0300

    654

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://restcountries.com/v2/name';
+const BASE_URL = 'https://restcountries.com/v3.1/name';
 
 export const fetchCountries = async (searchQuery) => {
   if (typeof searchQuery !== 'string' || searchQuery.length === 0) {
@@ -8,7 +8,7 @@ export const fetchCountries = async (searchQuery) => {
   const encodedSearchQuery = encodeURIComponent(searchQuery);
 
   try {
-    const response = await fetch(`${BASE_URL}/${encodedSearchQuery}?fields=name,flags.svg,capital,population,languages`);
+    const response = await fetch(`${BASE_URL}/${encodedSearchQuery}?fields=name,flags,capital,population,languages`);
     const data = await response.json();
     return data;
   } catch (error) {
